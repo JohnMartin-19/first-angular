4b367459cae8528465f00d197cc89bd37fd1bd96
John <johnnkonge2020@gmail.com>
2023-12-27 19:57:36 +0300

    added a service

---
+++ src/app/housing.service.ts
@@ -0,0 +1,122 @@
+import { Injectable } from '@angular/core';
+import {HousingLocation} from './housing_location/locationList';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+
+  protected housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: 'Green Court',
+      city: 'NAKURU',
+      state: 'NA',
+      photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 1,
+      name: 'Green Gardens',
+      city: 'NAKURU',
+      state: 'NA',
+      photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      id: 2,
+      name: 'JUMBLE HOUSE',
+      city: 'GILGIL',
+      state: 'GIL',
+      photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    },
+    {
+      id: 3,
+      name: 'HOUSY HOME',
+      city: 'GILGIL',
+      state: 'GIL',
+      photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 4,
+      name: 'MILIMANI HAPPY HOMES ',
+      city: 'NAKURU',
+      state: 'NA',
+      photo: `${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg`,
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 5,
+      name: 'MAMA MBURU HOMES',
+      city: 'NAKURU',
+      state: 'NA',
+      photo: `${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg`,
+      availableUnits: 2,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 6,
+      name: 'OLIVE ROAD HOMES',
+      city: 'GILGIL',
+      state: 'GIL',
+      photo: `${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg`,
+      availableUnits: 5,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 7,
+      name: 'COOL SHADE GARDENS',
+      city: 'GILGIL',
+      state: 'GIL',
+      photo: `${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg`,
+      availableUnits: 2,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 8,
+      name: 'NAMANGA DRIVE HOMES',
+      city: 'NAKURU',
+      state: 'NA',
+      photo: `${this.baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg`,
+      availableUnits: 10,
+      wifi: false,
+      laundry: false,
+    },
+    {
+      id: 9,
+      name: 'NAX VEGAS HOMES',
+      city: 'NAKURU',
+      state: 'NA',
+      photo: `${this.baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg`,
+      availableUnits: 6,
+      wifi: true,
+      laundry: true,
+    },
+  ];
+
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+  }
+
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
+  }
+}
+
