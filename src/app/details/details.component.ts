17727e50877f9cc764af1ad4dc9d13690b0620ea
John <johnnkonge2020@gmail.com>
2023-12-29 12:03:54 +0300

    updating details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {ActivatedRoute} from '@angular/router';
+import {HousingService} from '../housing.service';
+import {HousingLocation} from '../housing_location/locationList'
 @Component({
   selector: 'app-details',
   standalone: true,
